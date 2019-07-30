7204c618f7242c18e58adeb48b04d2193d1977c7
Brian Doane <brianmdoane@gmail.com>
2019-07-30 17:20:05 -0500

    Importing your own files

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const fs = require('fs')
+const getNotes = require('./notes.js')
 
-// Will create and write to file.  Changing text will overwrite.
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
-// Will append to original text.
-fs.appendFileSync('notes.txt', ' And then this guy added to it')
+const msg = getNotes()
+
+console.log(msg)
