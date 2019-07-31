58cf58a2333a0eeb1bb5ee9d47db2b26b4b0803f
Brian Doane <brianmdoane@gmail.com>
2019-07-31 11:32:25 -0500

    Argument parsing

--- app.js
+++ app.js
@@ -5,15 +5,58 @@ const getNotes = require('./notes.js')
 // Customize yargs version
 yargs.version('1.1.0')
 
+// node app.js --help to see commands
 // Create add command
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    console.log(`Title: ${argv.title}`)
+    console.log(`Body: ${argv.body}`)
+  }
+})
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
   handler: function () {
-    console.log('Adding a new note!')
+    console.log('Removing a note.')
+  }
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+    console.log('Reading a note!')
+  }
+})
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List a note',
+  handler: function () {
+    console.log('Listing a note.')
   }
 })
 
 // add, remove, read, list
-console.log(yargs.argv)
+// console.log(yargs.argv)
+
+yargs.parse()
 
