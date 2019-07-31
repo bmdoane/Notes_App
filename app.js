91b6c19d9d81a2b5975cb9151454be76478e84ad
Brian Doane <brianmdoane@gmail.com>
2019-07-31 10:25:05 -0500

    process.argv

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
+// Process.argv returns an array with path to node executable, path to app.js file and then value(s) after script in command line.  node app.js value(s)
+const command = process.argv[2]
 
-console.log(chalk.green('Monty\'s money is green'))
-console.log(chalk.red.inverse.bold('Homer loves Spider Pig'))
+if (command === 'add') {
+  console.log('Adding note!')
+} else if (command === 'remove') {
+  console.log('Removing note!')
+}
 
