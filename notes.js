38d53abfd2de91865ee0a44d85242055f394ab3d
Brian Doane <brianmdoane@gmail.com>
2019-07-31 16:03:19 -0500

    Adding notes flagged from CL to notes.json

--- notes.js
+++ notes.js
@@ -1,5 +1,47 @@
+const fs = require('fs')
+
 const getNotes = () => {
-  return "Lisa needs braces. Dental plan!"
+  return "Your notes..."
 }
 
-module.exports = getNotes
+const addNote = (title, body) => {
+  // Load data
+  const notes = loadNotes()
+  const dupeNotes = notes.filter((note) => {
+    return note.title === title
+  })
+
+  if (dupeNotes.length === 0) {
+    // Add data to array
+    notes.push({
+      title: title,
+      body: body
+    })
+    // Write to file
+    saveNotes(notes)
+    console.log('New note added.')
+  } else {
+    console.log('Note title taken!')
+  }
+}
+
+const saveNotes = (notesArray) => {
+  const dataJSON = JSON.stringify(notesArray)
+  fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+  // Try catch for defensive code
+  try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+  } catch (e) {
+    return []
+  }
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+}
