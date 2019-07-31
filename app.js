07844ab2862eac88226be17e68d010a5b758f6dc
Brian Doane <brianmdoane@gmail.com>
2019-07-31 09:30:23 -0500

    Importing npm modules and printing in color to console.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
-
 console.log(msg)
+
+console.log(chalk.green('Monty\'s money is green'))
+console.log(chalk.blue.inverse.bold('Homer loves Spider Pig'))
+
